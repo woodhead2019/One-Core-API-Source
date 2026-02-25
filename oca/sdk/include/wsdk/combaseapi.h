3e24a0b629bc4d6d87e4afd721d84129f4928f6f
skulltrail <samuel_4186@homail.com>
2026-02-25 00:51:01 -0300

    - Update combaseapi.h; - Update psapi_new;

--- oca/sdk/include/wsdk/combaseapi.h
+++ oca/sdk/include/wsdk/combaseapi.h
@@ -26,7 +26,7 @@
 #include <stdlib.h>
 #endif
 
-//#include <objidlbase.h>
+#include <objidlbase.h>
 #include <guiddef.h>
 
 #ifndef INITGUID
@@ -36,6 +36,10 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#define CLSCTX_ALL    (CLSCTX_INPROC_SERVER | CLSCTX_INPROC_HANDLER | CLSCTX_LOCAL_SERVER | CLSCTX_REMOTE_SERVER)
+#define CLSCTX_INPROC (CLSCTX_INPROC_SERVER | CLSCTX_INPROC_HANDLER)
+#define CLSCTX_SERVER (CLSCTX_INPROC_SERVER | CLSCTX_LOCAL_SERVER | CLSCTX_REMOTE_SERVER)
 
 typedef struct tagServerInformation
 {
@@ -51,7 +55,16 @@ enum AgileReferenceOptions
 };
 
 HRESULT WINAPI CoDecodeProxy(DWORD client_pid, UINT64 proxy_addr, ServerInformation *server_info);
-//HRESULT WINAPI RoGetAgileReference(enum AgileReferenceOptions options, REFIID riid, IUnknown *obj, IAgileReference **agile_reference);
+HRESULT WINAPI RoGetAgileReference(enum AgileReferenceOptions options, REFIID riid, IUnknown *obj, IAgileReference **agile_reference);
+
+#ifdef __cplusplus
+extern "C++" template<typename T> void **IID_PPV_ARGS_Helper(T **obj)
+{
+    (void)static_cast<IUnknown *>(*obj);
+    return reinterpret_cast<void **>(obj);
+}
+#define IID_PPV_ARGS(obj) __uuidof(**(obj)), IID_PPV_ARGS_Helper(obj)
+#endif /* __cplusplus */
 
 #ifdef __cplusplus
 }
